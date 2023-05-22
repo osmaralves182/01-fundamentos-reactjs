16173d9d49c6da5971771b7301f5838fe8fb26eb
osmaralves182 <osmar.alves@outlook.com>
2023-05-22 20:58:50 -0300

    Entendendo sobre as Propriedades no React

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,15 @@ export function App() {
 
   return (
     <div>
-      <Post />
+      <Post 
+        author="Osmar Alves" 
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, a dolorem. Mollitia nostrum hic laboriosam fuga placeat, temporibus porro consectetur sit accusantium nobis esse dolorum tenetur atque, sapiente, molestias illum!"
+      />
+
+      <Post 
+        author="Joaquim Silveira" 
+        content="Unde, a dolorem. Mollitia nostrum hic laboriosam fuga placeat, temporibus porro consectetur sit accusantium nobis esse dolorum tenetur atque, sapiente, molestias illum!"
+      />
     </div>
   )
 }
