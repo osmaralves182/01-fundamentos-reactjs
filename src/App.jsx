9bba9f78b53618357142efa8e8151dea9d9b8bad
osmaralves182 <osmar.alves@outlook.com>
2023-05-22 20:50:00 -0300

    Aprendendo sobre componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-function App() {
+import {Post} from "./Post";
+
+export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Post />
+    </div>
   )
 }
-
-export default App
