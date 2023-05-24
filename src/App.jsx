623fb848af5082ba106a9a273ea7fb89dde81fac
osmaralves182 <osmar.alves@outlook.com>
2023-05-24 19:36:09 -0300

    CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import {Post} from "./Post";
+import Header from "./components/Header";
+
+import './styles.css'
 
 export function App() {
 
   return (
     <div>
+      <Header />
+      
       <Post 
         author="Osmar Alves" 
         content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, a dolorem. Mollitia nostrum hic laboriosam fuga placeat, temporibus porro consectetur sit accusantium nobis esse dolorum tenetur atque, sapiente, molestias illum!"
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import styles from './Header.module.css'
+
+export default function Header(){
+  return (
+    <strong className={styles.header}>Ignite Feed</strong>
+  );
+}
