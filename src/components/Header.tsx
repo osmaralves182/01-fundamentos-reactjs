e28060f69de7d61aa8c3801948b039a293f38a96
osmaralves182 <osmar.alves@outlook.com>
2023-05-24 19:58:40 -0300

    Trabalhando no Component Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,12 @@
 import styles from './Header.module.css'
 
+import igniteLogo from '../assets/ignite-logo.svg'
+
 export default function Header(){
   return (
-    <strong className={styles.header}>Ignite Feed</strong>
+    <strong className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
+      Ignite Feed
+    </strong>
   );
 }
